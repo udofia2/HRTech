743ab701401dd12ef72c2ab2cb1877b1a3b22720
Odiong <enalsy22@gmail.com>
2026-05-24 18:45:02 +0100

    added Melo Associate details

---
+++ lib/melo/constants.ts
@@ -0,0 +1,16 @@
+export const COMPANY_NAME = "Melo Associates";
+export const COMPANY_SHORT = "Melo";
+export const FOUNDER_NAME = "Swati Garg";
+export const CONTACT_EMAIL = "swati@meloassociates.com";
+
+export const NETWORK_SIZE = 26000;
+export const FILL_RATE = 0.96;
+export const SUBMISSIONS_PER_HIRE = 6;
+
+export const NAMING_CONVENTION = {
+  prefix: "melo",
+  functionCase: "camelCase",
+  componentCase: "PascalCase",
+  fileCase: "kebab-case",
+};
+ 
